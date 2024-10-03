1b141b2062137845cee2febce3dc94cdcc533a7a
AtsuhiroSaitoh <64722928+saitoatuhiro@users.noreply.github.com>
2024-10-03 21:47:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.title('私の最初のStreamlitアプリ')
+
+st.write('これは、Streamlitで作成したシンプルなアプリです。')
+
+# スライダー
+value = st.slider('好きな数字を選んでください', 0, 100, 50)
+st.write('あなたの選んだ数字は', value, 'です。')
